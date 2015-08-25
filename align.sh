8acd1b4d0c64ddd25e632c7dc048946f167bf543
zhilbert <zhilbert@mit.edu>
2015-08-25 14:28:43 -0500

    added snpeff

--- align.sh
+++ align.sh
@@ -5,7 +5,7 @@
 STRAIN=MY18
 fastqc --threads 4 `ls data/fastq/$STRAIN*`
 # bwa
-bwa mem -t 4 reference/WS245/WS245.fa.gz data/fastq/$STRAIN_1.fq.gz data/fastq/$STRAIN_2.fq.gz | \
+bwa mem -t 4 reference/WS245/WS245.fa.gz data/fastq/${STRAIN}_1.fq.gz data/fastq/${STRAIN}_2.fq.gz | \
 samtools sort -O bam -T $STRAIN -@ 4 - > data/bam/$STRAIN.bam
 samtools index data/bam/$STRAIN.bam
 
@@ -14,10 +14,18 @@ fastqc data/bam/$STRAIN.bam
 
 # Variant calling v2
 REF=reference/WS245/WS245.fa.gz
-BAM=data/bam/MY10.bam
-STRAIN=MY10
+BAM=data/bam/${STRAIN}.bam
 chrom_regions=`samtools view -H $BAM | grep "\@SQ" | sed 's/^.*SN://g' | cut -f 1 | sed "s/$/.${STRAIN}.vcf.gz/g" | sed 's/^/data\/vcf\//g'`
 samtools view -H $BAM | grep "\@SQ" | sed 's/^.*SN://g' | cut -f 1 | xargs -I {} -n 1 -P 6 sh -c "samtools mpileup -BQ0 -uf $REF -r \"{}\" $BAM | bcftools call -O z -mv > data/vcf/\"{}\".$STRAIN.vcf.gz"
 bcftools concat -O z ${chrom_regions} > data/vcf/$STRAIN.vcf.gz
 bcftools index data/vcf/$STRAIN.vcf.gz
 rm ${chrom_regions}
+
+#stats
+ bcftools stats ${STRAIN}.vcf.gz > ${STRAIN}.stats.txt
+ plot-vcfstats -p ${STRAIN}/ ${STRAIN}.stats.txt
+
+#snpeff
+snpeff databases > databases.txt
+snpeff download WS241
+snpeff eff -no-downstream -no-intergenic -no-intron -no-upstream WS241 data/vcf/${STRAIN}.vcf.gz > ${STRAIN}.eff.vcf
